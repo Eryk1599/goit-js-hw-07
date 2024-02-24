4acd86d4f0f36f8492b8723cfe0797448379b300
Eryk1599 <127441215+Eryk1599@users.noreply.github.com>
2024-02-24 22:10:26 +0100

    final commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const galleryImages = galleryItems
+    .map(
+        (galleryItem) =>
+            `<div class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        alt="${galleryItem.description}"
+        data-source="${galleryItem.original}"
+        >
+        </img>
+        </a>
+     </div>`
+    )
+    .join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryImages);
+
+// ===========================================================================
+
+const lightbox = basicLightbox.create(`<img src="" />`, {
+    onShow: () => window.addEventListener("keydown", escape),
+    onClose: () => window.removeEventListener("keydown", escape),
+});
+
+function showLightbox(event) {
+    event.preventDefault();
+    const src = event.target.dataset.source;
+
+    if (!src) return;
+
+    lightbox.element().querySelector("img").src = src;
+    lightbox.show();
+}
+
+gallery.addEventListener("click", showLightbox);
+
+function escape(event) {
+    if (event.key !== "Escape") return;
+    lightbox.close();
+}
 
 console.log(galleryItems);
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://eryk1599.github.io/goit-js-hw-07/index.html
